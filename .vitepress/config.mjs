af8da27abff465c6f43df510eb208957b7502898
Slava Razum <razum@qruto.to>
2024-10-14 23:04:43 +0200

    Starting Web UI 💻 📱 🖥️

---
+++ .vitepress/config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Web UI",
+  description: "Well abstracted components using latest HTML & CSS capabilities."
+})
