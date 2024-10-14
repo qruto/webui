c46711ad0999f7d28700427b670bfd64ec226fc1
Slava Razum <razum@qruto.to>
2024-10-14 23:23:50 +0200

    Create Home page

---
+++ .vitepress/theme/Home.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { useData } from "vitepress";
+const { site } = useData();
+</script>
+<template>
+  <div>
+    <h1>{{ site.title }}</h1>
+    <p>{{ site.description }}</p>
+    <ul>
+      <li><a href="/markdown-examples.html">Markdown Examples</a></li>
+      <li><a href="/api-examples.html">API Examples</a></li>
+    </ul>
+  </div>
+</template>
--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -1,19 +1,13 @@
 <script setup>
-import { useData } from 'vitepress'
+import { useData } from "vitepress";
+import Home from "./Home.vue";
 
 // https://vitepress.dev/reference/runtime-api#usedata
-const { site, frontmatter } = useData()
+const { frontmatter } = useData();
 </script>
 
 <template>
-  <div v-if="frontmatter.home">
-    <h1>{{ site.title }}</h1>
-    <p>{{ site.description }}</p>
-    <ul>
-      <li><a href="/markdown-examples.html">Markdown Examples</a></li>
-      <li><a href="/api-examples.html">API Examples</a></li>
-    </ul>
-  </div>
+  <Home v-if="frontmatter.home" />
   <div v-else>
     <a href="/">Home</a>
     <Content />
