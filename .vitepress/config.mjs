75eb384fc45c1cafa30ba3fee3287c94605adf07
Slava Razum <razum@qruto.to>
2024-10-15 14:02:19 +0200

    Tailwind 4 alpha installed

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Web UI",
-  description: "Well abstracted components using latest HTML & CSS capabilities."
-})
+  description:
+    "Well abstracted components using latest HTML & CSS capabilities.",
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
--- .vitepress/theme/Home.vue
+++ .vitepress/theme/Home.vue
@@ -4,7 +4,7 @@ const { site } = useData();
 </script>
 <template>
   <div>
-    <h1>{{ site.title }}</h1>
+    <h1 class="text-red-500">{{ site.title }}</h1>
     <p>{{ site.description }}</p>
     <ul>
       <li><a href="/markdown-examples.html">Markdown Examples</a></li>
