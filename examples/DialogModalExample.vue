7d4c8fe9e6e52ed7d0ddf7c75e439f9a7bb6db5f
Slava Razum <razum@qruto.to>
2024-10-27 20:41:08 +0100

    Downgrade Tailwind to 3rd version

--- examples/DialogModalExample.vue
+++ examples/DialogModalExample.vue
@@ -20,7 +20,6 @@ function close() {
       :class="[
         // styles
         'px-10 pb-6 pt-10 bg-zinc-100 rounded-t-2xl shadow-[shadow:inset_0_2px_theme(colors.white),theme(boxShadow.lg)] dark:bg-zinc-700 dark:shadow-[shadow:inset_0_2px_theme(colors.white/5%),inset_0_-2px_theme(colors.black/7%),theme(boxShadow.lg)] sm:rounded-2xl',
-
         // reset browser defaults
         'max-w-none max-h-none',
         // place at the bottom center of the viewport
@@ -30,14 +29,14 @@ function close() {
         // enable complete transition
         'transition-[opacity,translate,transform,overlay,display] transition-allow-discrete duration-500',
         // mobile screen in / out transition
-        'translate-y-full open:starting:translate-y-full open:translate-y-0',
+        'translate-y-full open:[@starting-style]:translate-y-full open:translate-y-0',
         // wide screen in / out transition
-        'md:opacity-0 md:open:starting:opacity-0 md:open:opacity-100',
-        'md:open:starting:-translate-x-full md:translate-x-0 md:open:translate-x-0',
-        'md:-translate-y-full md:open:starting:translate-y-0',
+        'md:opacity-0 md:open:[@starting-style]:opacity-0 md:open:opacity-100',
+        'md:open:[@starting-style]:-translate-x-full md:translate-x-0 md:open:translate-x-0',
+        'md:-translate-y-full md:open:[@starting-style]:translate-y-0',
         // backdrop transition
         'backdrop:transition-[display,overlay,background-color] backdrop:transition-allow-discrete backdrop:duration-500',
-        'open:backdrop:bg-black/40 backdrop:open:starting:bg-black/0 backdrop:bg-black/0',
+        'open:backdrop:bg-black/40 open:backdrop:[@starting-style]:bg-black/0 backdrop:bg-black/0',
       ]"
       :open="open"
       @close="close"
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./.vitepress/**/*.{vue,js}', './examples/**/*.{vue,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
