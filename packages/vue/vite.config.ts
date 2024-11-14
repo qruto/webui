c5af4f95345f9d91ea576fdb1c5a554b94e24178
Slava Razum <razum@qruto.to>
2024-11-14 22:47:03 +0100

    wip

--- packages/vue/vite.config.ts
+++ packages/vue/vite.config.ts
@@ -1,25 +1,38 @@
-/// <reference types="vitest" />
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts';
 import packageJson from './package.json';
 
 const packageName = packageJson.name.split('/').pop() || packageJson.name;
-const toPascalCase = (string) =>
-  string
-    .match(/[a-z]+/gi)
-    .map((word) => word.charAt(0).toUpperCase() + word.substr(1).toLowerCase())
+const toPascalCase = (string: string) =>
+  (string.match(/[a-z]+/gi) || [])
+    .map((word) => word.charAt(0).toUpperCase() + word.substring(1).toLowerCase())
     .join('');
+
+console.log('here', toPascalCase(packageName))
 
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       formats: ['es', 'cjs', 'umd', 'iife'],
-      name: toPascalCase(packageName),
+      name: 'WebUI',
       fileName: 'webui'
     },
+    sourcemap: true,
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
   },
-  plugins: [dts({ rollupTypes: true })],
-  test: {},
+  plugins: [vue(), dts({
+    tsconfigPath: './tsconfig.app.json',
+    rollupTypes: true,
+  })],
+  // test: {},
 });
