29b771a0f2059160b01f36faab4d376b1e97a7db
Slava Razum <razum@qruto.to>
2024-11-18 18:22:21 +0100

    formatting

--- packages/vue/src/utils.ts
+++ packages/vue/src/utils.ts
@@ -1,57 +1,57 @@
-import { useMediaQuery } from '@vueuse/core';
+import { useMediaQuery } from '@vueuse/core'
 
 export function isExternal(url: string) {
-  const currentUrl = new URL(window.location.href);
+  const currentUrl = new URL(window.location.href)
 
   try {
-    const targetUrl = new URL(url);
+    const targetUrl = new URL(url)
 
-    return targetUrl.hostname !== currentUrl.hostname;
+    return targetUrl.hostname !== currentUrl.hostname
   } catch {
-    return false;
+    return false
   }
 }
 
 export function query(variant: string) {
-  let mediaQuery = `(min-width: var(--breakpoint-${variant}))`;
+  let mediaQuery = `(min-width: var(--breakpoint-${variant}))`
 
   if (variant.startsWith('max-')) {
-    variant = variant.substring(4);
+    variant = variant.substring(4)
 
-    mediaQuery = `not all and (min-width: var(--breakpoint-${variant}))`;
+    mediaQuery = `not all and (min-width: var(--breakpoint-${variant}))`
   }
 
   if (variant === 'dark') {
-    mediaQuery = '(prefers-color-scheme: dark)';
+    mediaQuery = '(prefers-color-scheme: dark)'
   }
 
   if (variant === 'portrait') {
-    mediaQuery = '(orientation: portrait)';
+    mediaQuery = '(orientation: portrait)'
   }
 
   if (variant === 'landscape') {
-    mediaQuery = '(orientation: landscape)';
+    mediaQuery = '(orientation: landscape)'
   }
 
   if (variant === 'motion-safe') {
-    mediaQuery = '(prefers-reduced-motion: no-preference)';
+    mediaQuery = '(prefers-reduced-motion: no-preference)'
   }
 
   if (variant === 'motion-reduce') {
-    mediaQuery = '(prefers-reduced-motion: reduce)';
+    mediaQuery = '(prefers-reduced-motion: reduce)'
   }
 
   if (variant === 'contrast-more') {
-    mediaQuery = '(prefers-contrast: more)';
+    mediaQuery = '(prefers-contrast: more)'
   }
 
   if (variant === 'contrast-less') {
-    mediaQuery = '(prefers-contrast: less)';
+    mediaQuery = '(prefers-contrast: less)'
   }
 
   if (variant === 'print') {
-    mediaQuery = 'print';
+    mediaQuery = 'print'
   }
 
-  return useMediaQuery(mediaQuery);
+  return useMediaQuery(mediaQuery)
 }
