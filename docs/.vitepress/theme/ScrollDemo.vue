212a5a25bcb9072b9f3ba4ebe71055c65bf9b013
Slava Razum <razum@qruto.to>
2025-01-25 20:26:46 +0100

    feat: scroll-timeline enabled

---
+++ docs/.vitepress/theme/ScrollDemo.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="flex h-[30dvh] flex-col justify-between">
+    <header class="space-y-4 text-center text-3xl text-zinc-800 dark:text-zinc-200">
+      <div>
+        scroll <span class="inline-block animate-bounce text-xl dark:text-zinc-400">↓</span>
+      </div>
+      <div class="pointer-fine:inline hidden">🖱️</div>
+      <div class="pointer-coarse:inline hidden">👆</div>
+    </header>
+    <p class="appearing">the main way users navigate content</p>
+  </section>
+</template>
+
+<style>
+.appearing {
+  animation-timeline: view(block 20% 60%);
+
+  animation-name: appear;
+  animation-fill-mode: both;
+  animation-timing-function: linear;
+}
+
+@keyframes appear {
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+    transform: translate(0, -50px);
+  }
+}
+</style>
