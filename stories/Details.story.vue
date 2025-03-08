325370af6f02ce50975f57c8c5ced581a81c7c46
Slava Razum <razum@qruto.to>
2025-03-08 01:20:36 +0100

    feat: details story

--- stories/Details.story.vue
+++ stories/Details.story.vue
@@ -1,32 +1,107 @@
 <script setup lang="ts">
-// import { MinusIcon, PlusIcon } from '@heroicons/vue/20/solid'
-import Details2 from 'components/Details2.vue'
+import { AnimatePresence, Motion } from 'motion-v'
+import { ref } from 'vue'
 
-// function appearance()
+// import { MinusIcon, PlusIcon } from '@heroicons/vue/20/solid'
+
+const open = ref(false)
+const visible = ref(false)
+
+async function toggle() {
+  if (!open.value) {
+    open.value = true
+    visible.value = true
+
+    return
+  }
+
+  visible.value = false
+}
 </script>
 
 <template>
-  <Story>
-    <Details2
-      ref="details"
-      class="dark:shadow-concave-dark shadow-concave rounded-3xl bg-zinc-200 px-8 py-4 dark:border-zinc-800 dark:bg-zinc-900 dark:text-zinc-500"
-      summary-class="flex gap-4 dark:text-zinc-200 rounded-t-3xl items-center justify-between"
-      :class="[
-        // 'mx-2 mb-0 translate-y-full',
-        // 'rounded-t-2xl opacity-100 open:translate-y-0 open:opacity-100 backdrop:open:bg-black/40',
-        // 'sm:mx-auto sm:mb-auto sm:rounded-2xl',
-        // 'sm:-translate-x-full sm:translate-x-0 sm:translate-y-full sm:opacity-0 sm:open:translate-x-0 sm:[@starting-style]:open:-translate-x-full sm:[@starting-style]:open:translate-y-0 sm:[@starting-style]:open:opacity-0',
-        // '[@starting-style]:open:translate-y-full [@starting-style]:backdrop:open:bg-black/0',
-      ]"
-      :open="open"
-      @toggle="onToggle"
-    >
-      <template #summary="{ open: openSummary }">
-        <strong class="font-bold">expand content</strong>
-        <PlusIcon v-if="!openSummary" class="size-5" />
-        <MinusIcon v-else class="size-5" />
-      </template>
-      <div class="mt-4">some details</div>
-    </Details2>
+  <Story title="details element">
+    <Variant title="motion js">
+      <details
+        :open="open"
+        class="focus-visible:outline-2 max-w-md focus-visible:outline-yellow-500"
+        ref="details"
+      >
+        <summary
+          ref="summary"
+          :class="[
+            '[&::-webkit-details-marker]:hidden list-none'
+          ]"
+          class="cursor-default flex items-center justify-between focus-visible:outline-2 focus-visible:outline-red-500"
+          @click.prevent="toggle"
+        >
+          summary
+          <span class="in-open:rotate-180 transition">↓</span>
+        </summary>
+        <AnimatePresence @exit-complete="open = false">
+          <Motion
+            v-show="visible"
+            as="div"
+            :initial="{ height: 0, opacity: 0 }"
+            :animate="{ height: 'auto', opacity: 1 }"
+            :exit="{ height: 0, opacity: 0 }"
+            :transition="{ duration: 0.2 }"
+            class="overflow-hidden"
+          >
+            No line break, paragraphs are too tight Excepteur id velit quis fugiat adipiscing nulla.
+            Aut id anim incididunt dolor ullamco ex veniam. Pariatur quis ad deserunt consequat qui.
+            Fugiat Lorem quis non amet laboris enim consequat magna eiusmod pariatur esse laboris.
+            Cupidatat proident culpa eu dolor ut veniam pariatur elit culpa elit dolore sunt et ea.
+            Sit do eiusmod velit. Aliquip fugiat mollit ea adipiscing velit consectetur sit irure
+            labore. Anim laboris ipsum ipsum. # Extra line break, too much space Excepteur id velit
+            quis fugiat adipiscing nulla. Aut id anim incididunt dolor ullamco ex veniam. Pariatur
+            quis ad deserunt consequat qui. Fugiat Lorem quis non amet laboris enim consequat magna
+            eiusmod pariatur esse laboris. Cupidatat proident culpa eu dolor ut veniam pariatur elit
+            culpa elit dolore sunt et ea. Sit do eiusmod velit. Aliquip fugiat mollit ea adipiscing
+            velit consectetur sit irure labore. Anim laboris ipsum ipsum.
+          </Motion>
+        </AnimatePresence>
+      </details>
+    </Variant>
+    <Variant title="motion css">
+      <details
+        class="focus-visible:outline-2 max-w-md focus-visible:outline-yellow-500"
+        ref="details"
+        :class="[
+          '[interpolate-size:allow-keywords]',
+          '[&::details-content]:overflow-hidden',
+          '[&::details-content]:opacity-0',
+          '[&::details-content]:[block-size:0]',
+          '[&::details-content]:transition-[block-size,opacity,content-visibility]',
+          '[&::details-content]:transition-discrete',
+          '[&::details-content]:ease-out',
+          '[&::details-content]:duration-200',
+          'open:[&::details-content]:[block-size:auto]',
+          'open:[&::details-content]:opacity-100',
+        ]"
+      >
+        <summary
+          ref="summary"
+          class="cursor-default flex items-center justify-between focus-visible:outline-2 focus-visible:outline-red-500"
+          :class="[
+            '[&::-webkit-details-marker]:hidden list-none'
+          ]"
+        >
+          summary
+          <span class="in-open:rotate-180 transition">↓</span>
+        </summary>
+        No line break, paragraphs are too tight Excepteur id velit quis fugiat adipiscing nulla. Aut
+        id anim incididunt dolor ullamco ex veniam. Pariatur quis ad deserunt consequat qui. Fugiat
+        Lorem quis non amet laboris enim consequat magna eiusmod pariatur esse laboris. Cupidatat
+        proident culpa eu dolor ut veniam pariatur elit culpa elit dolore sunt et ea. Sit do eiusmod
+        velit. Aliquip fugiat mollit ea adipiscing velit consectetur sit irure labore. Anim laboris
+        ipsum ipsum. # Extra line break, too much space Excepteur id velit quis fugiat adipiscing
+        nulla. Aut id anim incididunt dolor ullamco ex veniam. Pariatur quis ad deserunt consequat
+        qui. Fugiat Lorem quis non amet laboris enim consequat magna eiusmod pariatur esse laboris.
+        Cupidatat proident culpa eu dolor ut veniam pariatur elit culpa elit dolore sunt et ea. Sit
+        do eiusmod velit. Aliquip fugiat mollit ea adipiscing velit consectetur sit irure labore.
+        Anim laboris ipsum ipsum.
+      </details>
+    </Variant>
   </Story>
 </template>
